715849ba824a27d401c20d32fce7e9f4f0ec4f8a
summydev <adegbitesumayah@gmail.com>
2024-11-30 22:32:24 +0100

    commit

--- bloc_app2/lib/main.dart
+++ bloc_app2/lib/main.dart
@@ -10,14 +10,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => TodoCubit(),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Mini todo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
